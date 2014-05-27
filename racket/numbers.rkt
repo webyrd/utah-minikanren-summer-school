92e7ae541507fe4a8ff97a1ee276ee5bfe105e92
William E. Byrd <webyrd@gmail.com>
2014-05-27 09:43:14 -0600

    foo

---
+++ racket/numbers.rkt
@@ -0,0 +1,14 @@
+#lang racket
+(require (only-in "mk.rkt" run run*
+                  == =/=
+                  fresh eigen
+                  conde conda condu
+                  symbolo numbero ;; not-pairo
+                  absento
+                  prt project))
+
+(provide (all-defined-out))
+
+(include "numbers.scm")
+
+
