5f68e69a67b4c61cd95984e2e61b087c6316116e
William E. Byrd <webyrd@gmail.com>
2014-05-27 09:45:23 -0600

    added let-pair

---
+++ scheme/let-pair.scm
@@ -0,0 +1,8 @@
+
+(define-syntax let-pair
+  (syntax-rules ()
+    [(_ ((a . s) call) body)
+      (let ((tmp-res call))
+        (let ((a (car tmp-res))
+               (s (cdr tmp-res)))
+          body))]))
